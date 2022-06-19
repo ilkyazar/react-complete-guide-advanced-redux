0e45854c6bf5cc06bbe9e58c39e38ef5c7b8dba7
ilkyazar <ilkyaz.arabaci@gmail.com>
2022-06-19 14:47:09 +0300

    dispatch add to cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,15 +1,26 @@
+import { useSelector } from 'react-redux';
+
 import Card from '../UI/Card';
 import classes from './Cart.module.css';
 import CartItem from './CartItem';
 
 const Cart = (props) => {
+  const items = useSelector((state) => state.cart.items);
+
   return (
     <Card className={classes.cart}>
       <h2>Your Shopping Cart</h2>
       <ul>
-        <CartItem
-          item={{ title: 'Test Item', quantity: 3, total: 18, price: 6 }}
-        />
+        {items.map((i) => (
+          <CartItem
+            item={{
+              title: i.name,
+              quantity: i.quantity,
+              total: i.totalPrice,
+              price: i.price,
+            }}
+          />
+        ))}
       </ul>
     </Card>
   );
--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,9 +1,13 @@
 import { uiActions } from '../../store/ui-slice';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import classes from './CartButton.module.css';
 
 const CartButton = (props) => {
   const dispatch = useDispatch();
+
+  const cartQuantity = useSelector(
+    (state) => state.cart.totalQuantity
+  );
 
   const toggleCartHandler = () => {
     dispatch(uiActions.toggle());
@@ -12,7 +16,7 @@ const CartButton = (props) => {
   return (
     <button className={classes.button} onClick={toggleCartHandler}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{cartQuantity}</span>
     </button>
   );
 };
